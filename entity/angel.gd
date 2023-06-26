c6d5ce260d4fd1ace9ab4a2972b753ec885aee61
xorqu <vobhod4@mail.ru>
2023-06-26 20:51:13 +0300

    remove remesh

--- entity/angel.gd
+++ entity/angel.gd
@@ -1,11 +1,27 @@
 extends Node3D
 
+var t = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("angel_animation")
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	t += delta
+	$Node3D/Angel0.rotate_x(0.02)
+	
+	$Node3D/Angel1.rotate_x(+0.010)
+	$Node3D/Angel1.rotate_z(+0.004)
+	$Node3D/Angel1.rotate_y(-0.026)
+
+	$Node3D/Angel2.rotate_x(-0.022)
+	$Node3D/Angel2.rotate_z(+0.004)
+	$Node3D/Angel2.rotate_y(-0.024)
+	
+	$Node3D/Angel3.rotate_x(-0.002)
+	$Node3D/Angel3.rotate_z(-0.024)
+	$Node3D/Angel3.rotate_y(+0.006)
+
+	$Node3D.set_position(Vector3(0,sin(t)*1,0))
+	print($Node3D.get_position())
